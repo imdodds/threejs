99cc0e7fd6b497bca38cffdb2a5751c6c859bacd
Ian Dodds <ianmitchelldodds@gmail.com>
2023-08-10 16:10:11 -0400

    Initialize server

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -37,11 +37,30 @@ const Customizer = () => {
           readFile={readFile}
         />;
       case 'aipicker':
-        return <AIPicker />;
+        return <AIPicker
+          prompt={prompt}
+          setPrompt={setPrompt}
+          generatingImg={generatingImg}
+          handleSubmit={handleSubmit}
+        />;
       default:
         return null;
     }
   }
+
+  const handleSubmit = async (type) => {
+    if (!prompt) return alert('Please enter a prompt');
+
+    try {
+      // call backend to generate AI image
+      
+    } catch (error) {
+      alert(error)
+    } finally {
+      setGeneratingImg(false);
+      setActiveEditorTab('');
+    }
+  };
 
   const handleDecals = (type, result) => {
     const decalType = DecalTypes[type];
